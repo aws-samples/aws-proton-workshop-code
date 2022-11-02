3423729969f099188c3543eb26dc7d3ff993b02e
sbx_user1051 <sbx_user1051@169.254.19.253>
2022-11-02 22:19:56 +0000

    Update requested for Proton - environment/sample-vpc-environment-template

---
+++ sample-vpc-environment-template/proton.environment.variables.tf
@@ -0,0 +1,20 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-2:807080734664:environment/sample-vpc-environment-template
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+variable "environment" {
+  type = object({
+    inputs = map(string)
+    name   = string
+  })
+  default = null
+}
+
+variable "proton_tags" {
+  type    = map(string)
+  default = null
+}
